a03df3a99651317ce78707acc8be39e77f4b1cff
Gautam Jakhar <108416604+realgautamjakhar@users.noreply.github.com>
2023-04-14 19:05:37 +0530

    final

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const loading = () => {
   return (
-    <div className="flex items-center justify-center bg-gray-500  h-full">
+    <div className="flex items-center justify-center  h-full">
       <p>loading</p>
     </div>
   );
